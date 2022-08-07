89355ac041513dfbe62765b7ad252a3b92015b27
Theo Holroyd <tapsandsinkstheo@gmail.com>
2022-08-07 11:51:57 +0100

    made the hack a bit neater, but would still like a better solution

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ ports = serial.tools.list_ports.comports(include_links=False)
 for i, port in enumerate(ports):
     print(i+1, ". ", port)
 print("Enter the number of the port you would like to connect to by entering a number between 1 and ", i+1)
-port = ports[int(input())-1]
+# A sort of hack to get the port value as a string
+port = str(ports[int(input())-1]).split("(")[1].split(")")[0]
 print("Got the port ", port)
-port = str(port)
-port = port.split("(")[1]
-port = port.split(")")[0]
-print(port)
 ser = serial.Serial(port, 9600)
 while True:
     if ser.inWaiting() > 0:
