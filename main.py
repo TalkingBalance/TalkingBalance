29a1bdc7bc282ee5f1acaed596ef463e24c46144
Theo Holroyd <tapsandsinkstheo@gmail.com>
2022-08-07 11:41:53 +0100

    can select port but not connect to it

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import serial
 import time
-
-port = "COM3"
+import serial.tools.list_ports
+ports = serial.tools.list_ports.comports(include_links=False)
+for i, port in enumerate(ports):
+    print(i+1, ". ", port)
+print("Enter the number of the port you would like to connect to by entering a number between 1 and ", i+1)
+port = ports[int(input())-1]
+print("Got the port ", port)
 ser = serial.Serial(port, 9600)
 while True:
     if ser.inWaiting() > 0:
