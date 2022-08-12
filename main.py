8fdbd521f8ca3fc3cab6ea76df1681f59445b6b1
Theo Holroyd <tapsandsinkstheo@gmail.com>
2022-08-12 10:11:07 +0100

    ready for session

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import serial
 import time
 import serial.tools.list_ports
-import keyboard
 
-keyboard_press = ""
 ports = serial.tools.list_ports.comports(include_links=False)
 print("Welcome to Talking Scientific Scales!")
 print("Please select the port your scales are connected to from the following list. You are looking for something like USB-to-serial adaptor.")
@@ -15,8 +13,5 @@ print("Enter the number of the port you would like to connect to by entering a n
 port = str(ports[int(input())-1]).split("(")[1].split(")")[0]
 ser = serial.Serial(port, 9600)
 while True:
-    if keyboard.is_pressed("t") and keyboard_press != "t":
-        keyboard_press = "t"
-        print("You pressed key T")
     if ser.inWaiting() > 0:
         print(ser.readline().decode("utf-8".strip()))
