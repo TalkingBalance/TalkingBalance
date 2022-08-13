1c0b673a63a45cc60809ba45f76db227cb4965f2
Bmustill-rose <services@benmr.com>
2022-08-13 02:42:38 +0100

    select_port now validates input

--- utils.py
+++ utils.py
@@ -3,14 +3,20 @@ import serial.tools.list_ports
 def select_port():
     ports = serial.tools.list_ports.comports(include_links=False)
     if len(ports) == 1: return ports[0].name #No point in asking the user to choose a port if they only have one available
-    print("Please select the port your scales are connected to from the following list. You are looking for something like USB-to-serial adaptor.")
+    print("Please select the port your scales are connected too from the following list:")
     print("If your scales do not appear in the below list, disconnect and reconnect them and then restart this program.")
     for i, port in enumerate(ports):
-        print(i+1, ". ", port)
-    print("Enter the number of the port you would like to connect to by entering a number between 1 and ", i+1)
-    # A sort of hack to get the port value as a string
-    port = str(ports[int(input())-1]).split("(")[1].split(")")[0]
-    return port
+        print(f"{i+1}. {port.name}")
+    print(f"Enter the number of the port you would like to connect too by entering a number between 1 and {i+1}")
+    while True:
+        chosenPort = input()
+        if isValidPortNumber(ports, chosenPort): return ports[int(chosenPort)-1].name
+        print(f"{chosenPort} is not a valid port number. Please try again.")
+
+def isValidPortNumber(ports, chosenPort):
+    if not chosenPort.isnumeric(): return False
+    if 1 <= int(chosenPort) <= len(ports): return True
+    return False
 
 def send_command(ser, command, message):
     ser.write(str.encode(command))
