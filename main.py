9328e61b9c4c26f1eac904588a3e927c6ba3eb1d
Theo Holroyd <tapsandsinkstheo@gmail.com>
2022-08-25 10:11:51 +0100

    added hotkey for units, how can we say which units the user switches to?

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import keyboard
 port = utils.select_port()
 ser = serial.Serial(port, 9600)
 keyboard.add_hotkey("t", utils.send_command, args=[ser, "T", "Now tared."])
+keyboard.add_hotkey("u", utils.send_command, args=[ser, "U", "Switched units."])
+
 while True:
     if ser.inWaiting() > 0:
         print(ser.readline().decode("utf-8".strip()))
