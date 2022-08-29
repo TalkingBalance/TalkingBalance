410966c29971f23485e4e7335790c4139d54df44
Legendusmaximus <tapsandsinkstheo@gmail.com>
2022-08-29 08:04:22 +0100

    added more hotkeys

--- main.py
+++ main.py
@@ -15,8 +15,16 @@ except Exception as e:
     sys.exit()
 
 ser = serial.Serial(port, 9600)
+
 keyboard.add_hotkey("t", utils.send_command, args=[ser, "T", "Now tared."])
 keyboard.add_hotkey("u", utils.send_command, args=[ser, "U", "Switched units."])
+keyboard.add_hotkey("o", utils.send_command, args=[ser, "O", "Scales have been switched off."])
+keyboard.add_hotkey("m", utils.send_command, args=[ser, "M", ""])
+keyboard.add_hotkey("f", utils.send_command, args=[ser, "F", "Changing mode."])
+keyboard.add_hotkey("c", utils.send_command, args=[ser, "C", "Calibration started."])
+keyboard.add_hotkey("p", utils.send_command, args=[ser, "P", ""])
+
+
 print(f"Using {port}. If you haven't already please turn your scales on.")
 
 while True:
