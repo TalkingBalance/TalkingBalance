8ba27eff994fe37835326af8dd26a62ec5a97d50
Bmustill-rose <services@benmr.com>
2022-08-29 12:08:40 +0100

    Introduce basic scales class and move existing scales-related methods into it

---
+++ scales.py
@@ -0,0 +1,16 @@
+class Scales:
+    def __init__(self, ser):
+        self.ser = ser
+
+    def hasMessageToDisplay(self):
+        if self.ser.inWaiting() > 0:
+            return True
+        else:
+            return False
+
+    def readRaw(self):
+        return self.ser.readline().decode("utf-8".strip())
+    
+    def send_command(self, command, message):
+        self.ser.write(str.encode(command))
+        print(message)
