5a8dbdea84fbf8482058162c901e1f57de351533
Theo Holroyd <tapsandsinkstheo@gmail.com>
2022-09-01 09:50:45 +0100

    fixed grammar error in print statement in select_port

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import serial.tools.list_ports
 def select_port():
     ports = serial.tools.list_ports.comports(include_links=False)
     if len(ports) == 1: return ports[0].name #No point in asking the user to choose a port if they only have one available
-    print("Please select the port your scales are connected too from the following list:")
+    print("Please select the port your scales are connected too from the following list.")
     print("If your scales do not appear in the below list, disconnect and reconnect them and then restart this program.")
     for i, port in enumerate(ports):
         print(f"{i+1}. {port}")
@@ -18,12 +18,3 @@ def is_valid_port_number(ports, chosenPort):
     if 1 <= int(chosenPort) <= len(ports): return True
     return False
 
-def scales_turned_off_choice():
-    print("The scales have now been turned off. Would you like to exit? [y/n]")
-    exit = input()
-    if exit == "y" or exit == "yes":
-        print("Exiting...")
-        import sys
-        sys.exit()
-    else:
-        print("The program will stay open until closed with Control+C")
